5af8e7ee94afa5b8d42af61918eba3d9b63d36f7
Davit Mujirishvili <mujirishvilidato6@gmail.com>
2026-02-12 00:25:25 +0400

    Added LoginPage logic

---
+++ Pages/LoginPage.ts
@@ -0,0 +1,15 @@
+import { Page } from '@playwright/test';
+
+export class LoginPage {
+  private page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async login(username: string, password: string) {
+    await this.page.fill('[data-test="username"]', username);
+    await this.page.fill('[data-test="password"]', password);
+    await this.page.click('[data-test="login-button"]');
+  }
+}
