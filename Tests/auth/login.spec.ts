5826d9b543a200beffdcaa04cf631e0e67f1ad1f
Davit Mujirishvili <mujirishvilidato6@gmail.com>
2026-02-12 00:25:45 +0400

    Added LoginPage logic

--- Tests/auth/login.spec.ts
+++ Tests/auth/login.spec.ts
@@ -1,11 +1,14 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../../pages/LoginPage';
 
+const userName = 'standard_user'
+const password = 'secret_sauce'
+
 test('valid login', async ({ page }) => {
   const loginPage = new LoginPage(page);
 
   await page.goto('https://www.saucedemo.com/');
-  await loginPage.login('standard_user', 'secret_sauce');
+  await loginPage.login(userName, password);
 
   await expect(page).toHaveURL(/inventory/);
 });
