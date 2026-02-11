2420e874c11e8f51543504f7dfb311df5999a6d3
Davit Mujirishvili <mujirishvilidato6@gmail.com>
2026-02-12 00:37:22 +0400

    Added LoginPage logic

--- Pages/LoginPage.ts
+++ Pages/LoginPage.ts
@@ -3,13 +3,20 @@ import { Page } from '@playwright/test';
 export class LoginPage {
   private page: Page;
 
+  private usernameInput;
+  private passwordInput;
+  private loginButton;
+
   constructor(page: Page) {
     this.page = page;
+    this.usernameInput = page.getByPlaceholder("Username");
+    this.passwordInput = page.getByPlaceholder("Password");
+    this.loginButton = page.getByRole('button', {name: 'Login'});
   }
 
   async login(username: string, password: string) {
-    await this.page.fill('[data-test="username"]', username);
-    await this.page.fill('[data-test="password"]', password);
-    await this.page.click('[data-test="login-button"]');
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
   }
 }
